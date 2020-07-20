045a73155c112d86c5f294ab266dd6a510dd3021
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-20 19:50:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,7 @@ class Dog
     Dog.new(id: result[0], name: result[1], breed: result[2])
   end 
 
-  def self.find_or_create_by(row) 
+  def self.find_or_create_by(name:, breed:) 
     sql = "SELECT * FROM dogs"
   end 
 end 
