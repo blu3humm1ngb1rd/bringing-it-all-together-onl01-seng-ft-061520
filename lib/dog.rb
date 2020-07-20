955d9c6118bdcdee04b39c1d3a23c910829d8fd7
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-20 19:55:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,7 +67,7 @@ class Dog
       dog_data = dog[0]
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else 
-      dog = dog.create(name: name, breed: breed)
+      dog = dog.create()
     end 
     dog 
   end 
