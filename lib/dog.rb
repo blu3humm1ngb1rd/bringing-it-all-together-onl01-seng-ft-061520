8cb2dcd27f1de60ae3e1e6538522e00cd17ba84f
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-20 20:06:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,7 +73,7 @@ class Dog
   end 
   
   def self.find_by_name(name)
-   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name)
+   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name)[1]
    dog
   end 
   
