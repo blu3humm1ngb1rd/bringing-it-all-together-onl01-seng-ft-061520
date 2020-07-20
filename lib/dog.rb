7170ecf6b7c499f8afe795f2195b297df94143f9
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-20 19:52:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,7 +62,7 @@ class Dog
   end 
 
   def self.find_or_create_by(name:, breed:) 
-    
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     sql = "SELECT * FROM dogs"
   end 
 end 
